c74b459613b2a0c3bb09f3c6858d64773b75c19e
surajDongre-16 <surajdongre8@gmail.com>
2022-08-24 00:46:04 +0530

    more button model done

--- sloppy-impulse-7268/frontend/src/components/Dashboard/Navbar.jsx
+++ sloppy-impulse-7268/frontend/src/components/Dashboard/Navbar.jsx
@@ -6,8 +6,8 @@ import NavTop from "./NavTop";
 const Navbar = () => {
   return (
     <Box>
-      <Box bg="#3b8302" h="125px">
-        <Container maxW={"75rem"} border="1px solid white">
+      <Box bg="#3b8302" h="125px" border={"1px solid #3b8302"}>
+        <Container maxW={"75rem"}>
           <NavTop />
           <NavBottom />
         </Container>
