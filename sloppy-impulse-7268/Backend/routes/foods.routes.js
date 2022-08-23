320c142e7208fb410fb35382ff7a0848566f8b3f
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-08-24 00:18:41 +0530

    routes done

--- sloppy-impulse-7268/Backend/routes/foods.routes.js
+++ sloppy-impulse-7268/Backend/routes/foods.routes.js
@@ -31,10 +31,52 @@ foodRouter.get("/getallfoods", async (req, res) => {
     res.send({ "message": "get data successfully", dataGet });
 })
 
+// Breakfast data
+
 
 foodRouter.get("/getbreakfast", async (req, res) => {
 
+    const breakfast = await foodModel.find({ food: "breakfast" });
+    // console.log(breakfast);
+
+    res.send({ "message": "breakfast data get", "count": breakfast.length, breakfast })
+
 })
+
+
+// lunch data
+
+foodRouter.get("/getlunch", async (req, res) => {
+
+    const lunch = await foodModel.find({ food: "lunch" });
+    // console.log(lunch);
+
+    res.send({ "message": "lunch data get", "count": lunch.length, lunch })
+
+})
+
+// dinner
+
+foodRouter.get("/getdineer", async (req, res) => {
+
+    const dinner = await foodModel.find({ food: "dinner" });
+    // console.log(lunch);
+
+    res.send({ "message": "dinner data get", "count": dinner.length, dinner })
+
+})
+
+// snacks
+
+foodRouter.get("/getsnacks", async (req, res) => {
+
+    const snacks = await foodModel.find({ food: "snacks" });
+    // console.log(lunch);
+
+    res.send({ "message": "snacks data get", "count": snacks.length, snacks })
+
+})
+
 
 
 
