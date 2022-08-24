13a17abd1baa33a7151b2ec120a75f1b348161e8
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-08-24 11:11:15 +0530

    start

---
+++ sloppy-impulse-7268/frontend/src/Pages/Foodpage/FoodMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FoodMain() {
+    return (
+        <div>
+            <h1>Home Food</h1>
+        </div>
+    )
+}
+
+export default FoodMain
--- sloppy-impulse-7268/frontend/src/Pages/MainRoutes.jsx
+++ sloppy-impulse-7268/frontend/src/Pages/MainRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import FoodMain from "./Foodpage/FoodMain";
 import Homepage from "./Homepage";
 
 
@@ -7,7 +8,8 @@ const MainRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage />}/>    
+        <Route path="/" element={<Homepage />} />
+        <Route path="/food" element={<FoodMain />} />
       </Routes>
     </div>
   );
