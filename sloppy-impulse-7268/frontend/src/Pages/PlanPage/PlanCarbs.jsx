42240a9f95c74b23d4a134127127f7962252459d
rushi2784 <96285307+rushi2784@users.noreply.github.com>
2022-08-25 15:18:19 +0530

    plan

---
+++ sloppy-impulse-7268/frontend/src/Pages/PlanPage/PlanCarbs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlanCarbs() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PlanCarbs
--- sloppy-impulse-7268/frontend/src/Pages/PlanPage/PlanRoutes.jsx
+++ sloppy-impulse-7268/frontend/src/Pages/PlanPage/PlanRoutes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import PlanSidebar from './PlanSidebar';
+import PlanWeight from './PlanWeight';
 function PlanRoutes() {
     return (
         <div>
             <Routes>
 
                 <Route path="/planhome" element={<PlanSidebar />} />
-                {/* <Route path="/food" element={<FoodMain />} /> */}
+                <Route path="/planweight" element={<PlanWeight />} />
 
                 {/* <Route path="/" element={<Homepage />}/>   */}
                 {/* <Route path="/dashboard" element={<Dashboard/>}  /> */}
