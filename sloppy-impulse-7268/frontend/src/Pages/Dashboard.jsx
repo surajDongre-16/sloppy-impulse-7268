072255ecc53cc7e14e5207af8b1ae5bf88c3c3d0
surajDongre-16 <surajdongre8@gmail.com>
2022-08-25 20:00:51 +0530

    footer section done

--- sloppy-impulse-7268/frontend/src/Pages/Dashboard.jsx
+++ sloppy-impulse-7268/frontend/src/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react'
+import Footer from '../components/Dashboard/Footer';
 import MiddleSection from '../components/Dashboard/MiddleSection';
 import Navbar from '../components/Dashboard/Navbar'
 
@@ -9,6 +10,7 @@ const Dashboard = () => {
     <Box bg="#f0f0f0">
       <Navbar />
       <MiddleSection />
+      <Footer/>
     </Box>
   );
 }
