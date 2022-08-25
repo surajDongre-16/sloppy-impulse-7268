42e7b439d0c6ede5e3fd0d927d02bb19703c2940
DeepanshuChugh <deepanshuchugh24@gmail.com>
2022-08-25 18:09:17 +0530

    7thAnd8thComponentWithFooter

--- sloppy-impulse-7268/frontend/src/Pages/MainRoutes.jsx
+++ sloppy-impulse-7268/frontend/src/Pages/MainRoutes.jsx
@@ -3,13 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Homepage from "./Homepage";
 
-
 const MainRoutes = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage />}/>  
-        <Route path="/dashboard" element={<Dashboard/>}  />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
