ac7c6a84d93c3a6ddece1446663930c88420a19e
DeepanshuChugh <deepanshuchugh24@gmail.com>
2022-08-28 10:08:56 +0530

    f

--- sloppy-impulse-7268/frontend/src/Pages/Library.jsx
+++ sloppy-impulse-7268/frontend/src/Pages/Library.jsx
@@ -2,17 +2,25 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import Cards from "../components/Library/Cards";
 import Menus from "../components/Library/Menus";
+import Navbar from "./Navbar";
 
 const Library = () => {
   return (
-    <Box display="flex">
-      <Box h="auto" w="25rem">
-        <Menus />
+    <>
+      <Navbar />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Box display="flex">
+        <Box h="auto" w="25rem">
+          <Menus />
+        </Box>
+        <Box h="auto" w="75rem">
+          <Cards />
+        </Box>
       </Box>
-      <Box h="auto" w="75rem">
-        <Cards/>
-      </Box>
-    </Box>
+    </>
   );
 };
 
