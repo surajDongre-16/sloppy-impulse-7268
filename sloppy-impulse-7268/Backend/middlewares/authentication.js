69aabb3ba34e3319b8fd208f031fb1c662ff70aa
surajDongre-16 <surajdongre8@gmail.com>
2022-10-03 15:21:11 +0530

    Fixed: resposne messages

--- sloppy-impulse-7268/Backend/middlewares/authentication.js
+++ sloppy-impulse-7268/Backend/middlewares/authentication.js
@@ -2,18 +2,9 @@ const jwt = require('jsonwebtoken');
 require("dotenv").config();
 
 const authentication = (req, res, next) => {
-    // Such logging messages don't help. I'm guessing it was added for debugging purposes but never removed.
-    // console.log(req.headers,"reqHead")
+  
     if(!req.headers.authorization){
-        // This is not a good way to return error responses. APIs should conform to REST principles.
-        // All error response should be formatted in a certain way so that the client knows what to expect.
-        // A proper json object with error message and any other relevant information must be sent back
-        // with an appropriate status code. For authentication issues status code 401 should be used.
         // Read about status codes here - https://restfulapi.net/http-status-codes/ 
-        // It should be something like -
-        // res.status(401).send({ error: "Authorization header was missing" });
-        // Same applies below. Using the same error message doesn't help identify what the client end
-        // issue was.
         return res
           .status(401)
           .send({ error: "Authorization header was missing" });
@@ -23,7 +14,7 @@ const authentication = (req, res, next) => {
       if (err) {
         return res
           .status(401)
-          .send({ error: "Authorization header was missing" });
+          .send({ error: "Error ocuured during authentication" });
       }
       // console.log(decoded);
       // Instead of adding the requesting user's information to the body, it should be added on a new
