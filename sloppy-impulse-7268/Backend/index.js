e0ea6b3bc70ad1da9a3e8993b045e2e54aef7d2d
surajDongre-16 <surajdongre8@gmail.com>
2022-10-03 14:58:30 +0530

    Fixed: mongoose error

--- sloppy-impulse-7268/Backend/index.js
+++ sloppy-impulse-7268/Backend/index.js
@@ -1,45 +1,30 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
+const connection = require("./config/db");
+const weightRoutes = require("./routes/weights.routes");
+const foodRouter = require("./routes/foods.routes");
+const userController = require("./routes/user.routes");
+const authentication = require("./middlewares/authentication");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-
-// All require statements should be together at the top.
-const connection = require("./config/db");
-
-const weightRoutes = require("./routes/weights.routes");
-const foodRouter = require("./routes/foods.routes");
-const userController = require("./routes/user.routes")
-const authentication = require("./middlewares/authentication")
-
-
 app.use("/weight", weightRoutes);
-app.use("/user", userController)
-app.use(authentication)
+app.use("/user", userController);
+app.use(authentication);
 app.use("/food", foodRouter);
 
-
-// What is this for?
-app.get("/", (req, res) => {
-  res.send("home page");
-});
-
-
-app.listen(process.env.PORT, async () => {
+app.listen(process.env.PORT, () => {
   try {
-    // This is very strange. Await is to be used with async operations. You can't await on a module export.
-    // Basically this await will have no effect.
-    await connection;
+    connection;
     console.log("Db connnected");
   } catch (err) {
     // If connecting to the DB fails, can the app run reliably? This should ideally be retried in the hopes that
     // it might be a transient error which gets fixed on a few retries (say 5) at most. If it still fails the app
     // should crash and there should be a process manager like https://pm2.keymetrics.io/ that restarts the process.
-    console.log("error connecting to db");
-    console.log(err);
+    console.log("error connecting to db", err);
   }
-  // This is redundant and not a helpful log message. Usually node will add its own start up logs.
-  console.log("listening on 8080");
 });
