1a2d401bd2d10c80546f351e9ac940e6e38f88aa
surajDongre-16 <surajdongre8@gmail.com>
2022-10-03 16:01:10 +0530

    update: removed dead codes

--- sloppy-impulse-7268/frontend/src/Pages/MainRoutes.jsx
+++ sloppy-impulse-7268/frontend/src/Pages/MainRoutes.jsx
@@ -1,34 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
-// import FoodMain from "./Foodpage/FoodMain";
-
 import Dashboard from "./Dashboard";
-
 import Homepage from "./Homepage";
-
-// import Plan from "./Subpages/Plan";
-
-// Lots of unused imports in this file. Dead code is not good. Increases maintenance cost of code.
-
-import Exercise from "./Subpages/Exercise";
-import Analysis from "./Subpages/Analysis";
-import Community from "./Subpages/Community";
-import Setting from "./Subpages/Setting";
-
-
-import PlanWeight from "./PlanPage/PlanWeight";
-
-// import Plan from "./Subpages/Plan";
-// import Food from "./Subpages/Food";
-// import Exercise from "./Subpages/Exercise";
-// import Analysis from "./Subpages/Analysis";
-// import Community from "./Subpages/Community";
-// import Setting from "./Subpages/Setting";
-
-// import PlanSidebar from "./PlanPage/PlanSidebar";
-// import PlanWeight from "./PlanPage/PlanWeight";
-
 import PlanCarbs from "./PlanPage/PlanCarbs";
 import PlanExericse from "./PlanPage/PlanExericse";
 import PlanNutrient from "./PlanPage/PlanNutrient";
@@ -45,13 +18,6 @@ const MainRoutes = () => {
         <Route path="/dashboard/:id" element={<Dashboard />} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/signin' element={<LoginPage />} />
-        {/* <Route path="/dashboard/plan" element={<PlanWeight />} /> */}
-        {/* <Route path="/dashboard/food" element={<FoodMain />} />
-        <Route path="/dashboard/exercise" element={<Exercise />} />
-        <Route path="/dashboard/analysis" element={<AnalysisPage />} />
-        <Route path="/dashboard/community" element={<Community />} />
-  <Route path="/dashboard/settings" element={<Setting />} /> */}
-        {/* Plan Routes */}
         <Route path="/plancarbs" element={<PlanCarbs />} />
         <Route path="/planexericse" element={<PlanExericse />} />
         <Route path="/plannutrient" element={<PlanNutrient />} />
